b66d5fbba39816d9e027b2b4428bea4f9a69cadd
SBOAAAA <fun_pu@hotmail.com>
2022-11-11 14:34:14 +0700

    edit

---
+++ spec/gameslot-roma-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GameslotRomaView from '../lib/gameslot-roma-view';
+
+describe('GameslotRomaView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
